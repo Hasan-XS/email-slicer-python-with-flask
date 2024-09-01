ea952409daae29077b41d4d5fcb350841b24df79
Hasan-XS <ha3an.majidii@gmail.com>
2024-09-02 02:36:25 +0330

    Done

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-from flask import Flask, render_template, request
+from sqlalchemy.exc import IntegrityError
+from flask import Flask, render_template, request, redirect, url_for
 from config import SQLALCHEMY_DATABASE_URI
 from extention import *
+from model.email import Email
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+migrate = Migrate(app, db)
 db.init_app(app)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        email = request.form.get("email")
-        print(email)
-        return render_template("index.html", emails=email)
+        email = request.form.get("email", None)
         
+        username = email[:email.index('@')]
+        domain = email[email.index('@') + 1:]
+
+        email_user = Email(username=username, domain=domain, email=email)
+
+        try:
+            db.session.add(email_user)
+            db.session.commit()
+        except IntegrityError:
+            return "Not Uniq"
+
+        return redirect(url_for("home"))
     else:
-        return render_template("index.html")
+        email = Email.query.all()
+        return render_template("index.html", email=email) 
 
 
 with app.app_context():
--- model/email.py
+++ model/email.py
@@ -4,5 +4,8 @@ from extention import db, get_current_time
 class Email(db.Model):
     __tablename__ = "emails"
     id = Column(Integer, primary_key=True)
+    username = Column(String, unique=False, nullable=False)
+    domain = Column(String,unique=False, nullable=False, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
     date_create = Column(String(12), default=get_current_time)
+
