ec200738b4be5e5e4e8c889eb9e0d4c95f7efc6c
Hasan-XS <ha3an.majidii@gmail.com>
2024-09-01 19:51:23 +0330

    Create database sqllite

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
-
+from config import SQLALCHEMY_DATABASE_URI
+from extention import *
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+db.init_app(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -15,5 +19,7 @@ def home():
         return render_template("index.html")
 
 
+with app.app_context():
+    db.create_all()
 if __name__ == "__main__":
     app.run(debug=True)
